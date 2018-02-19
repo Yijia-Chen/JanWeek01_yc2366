6f421a98618a0737c63342fbd64fe21254adb329
greenwich95 <31546484+greenwich95@users.noreply.github.com>
2018-02-18 23:43:24 -0500

    Update vectorProduct.cpp

--- HackerPractice/FebWeek03/vectorProduct.cpp
+++ HackerPractice/FebWeek03/vectorProduct.cpp
@@ -20,7 +20,7 @@ int retrieveElement(int x, int y){
     int value[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
     int rowPtr[6] = {0,3,6,9,10,12};
     int colInd[12] ={0,1,4,0,1,2,1,2,4,3,0,4};
-    bool z = false;
+    bool z=false;
     
     for(int i=rowPtr[x-1]; i<=rowPtr[x]-1;i++){
         if(colInd[i] == y-1){
@@ -28,7 +28,7 @@ int retrieveElement(int x, int y){
             return value[i];
         }
     }
-    if(z==false) return 0;
+    return 0;
 }
 
 int productAx(int matrix[5][5], int vecx[5][1]){
@@ -43,6 +43,7 @@ int productAx(int matrix[5][5], int vecx[5][1]){
     return 0;
 }
 
+
 int main(){
     int i,j;
     
@@ -52,8 +53,8 @@ int main(){
     
     for(i=1;i<=5;i++){
         for(j=1;j<=5;j++){
-            vecb[j-1][0] = vecb[j-1][0]+retrieveElement(i, j)*vecx[j-1][0];
-            if(j==5)  cout<<vecb[j-1][0]<<endl;
+            vecb[i-1][0] += retrieveElement(i, j)*vecx[j-1][0];
+            if(j==5)  cout<<vecb[i-1][0]<<endl;
         }
     }
     productAx(matrix, vecx);
